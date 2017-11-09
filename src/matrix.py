6c0cd82c35688a8c086d7e5ae1c4209bebc8212c
fariz.tumbuan <13515050@std.stei.itb.ac.id>
2017-11-10 02:33:17 +0800

    Membuat kelas matrix dan fungsi kali matriks

---
+++ src/matrix.py
@@ -0,0 +1,31 @@
+class Matrix:
+    def __init__(self, rowSize, columnSize):
+        self.matrix = []
+
+        for i in range(columnSize):
+            aColumn = []
+            for j in range(rowSize):
+                aColumn += [None]
+
+            self.matrix += [aColumn]
+
+    def setValue(self, value, rowPos, colPos):
+        self.matrix[colPos][rowPos] = value
+
+def multiplyMatrix(m1, m2):
+    rowSize = len(m1[0])
+    columnSize = len(m2)
+    inBetween = len(m1)
+    temp = Matrix(rowSize, columnSize)
+    for i in range(rowSize):
+        for j in range(columnSize):
+            sum = 0
+            for k in range(inBetween):
+                sum += m1[k][i] * m2[j][k]
+            print("sum = " + str(sum))
+            temp.setValue(sum, i, j)
+
+    result = temp.matrix[:]
+    return result
+
+
